0a566356cc19cd5f1c653061860bc8a72e5c188f
Nikos Cocci <coccinikos@gmail.com>
2023-09-26 14:24:19 +0200

    project setup

---
+++ app/global_theme.tsx
@@ -0,0 +1,11 @@
+const theme = {
+  breakpoint: {
+    xs: `@media (max-width: 576px)`,
+    sm: `@media (max-width: 768px)`,
+    md: `@media (max-width: 992px)`,
+    lg: `@media (max-width: 1200px)`,
+    xl: `@media (max-width: 1400px)`
+  }
+}
+
+export { theme }
