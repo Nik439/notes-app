d0a61795dfbe06e73f38c7629fdf54b2244acff7
Nikos Cocci <coccinikos@gmail.com>
2023-09-28 16:01:46 +0200

    added animations to note component

--- app/components/note.tsx
+++ app/components/note.tsx
@@ -1,43 +1,86 @@
 'use client'
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { note } from "../models/note";
 import Link from "next/link";
 
 export default function Note({note}: {note: note}) {
 
   return (
-    <Link href={`/note/${note.id}`}>
-      <Container>
+    <Container href={`/note/${note.id}`}>
+      <Content>
+        <NoteBackground className="note_background"></NoteBackground>
         <Title>{note.title}</Title>
         <Text>{note.text}</Text>
-      </Container>
-    </Link>
-
+      </Content>
+    </Container>
   )
 }
 
+const clipAnimation = keyframes`
+  0% { clip-path: circle(0%); }
+  100% { clip-path: circle(100%); }
+`
 
-export const Container = styled.div`
+const bounceAnimation = keyframes`
+  0% { transform: scale(100%) }
+  50% { transform: scale(105%) }
+  100% { transform: scale(100%) }
+`
+
+export const Container = styled(Link)`
   height: 250px;
   width: 200px;
-  background-color: #fff;
-  margin: 8px;
+  background-color: #000;
   display: flex;
   align-items: center;
-  flex-direction: column;
-  padding: 5px 10px;
+  border-radius: 8px;
+  max-width: 100%;
+  overflow: hidden;
+  transform-origin: center;
+  animation: ${clipAnimation} 0.8s ease-out,  ${bounceAnimation} .4s ease .4s;
   border: 2px solid #000;
   border-bottom: 6px solid #000;
   border-right: 6px solid #000;
   border-radius: 8px;
-  max-width: calc(100% - 16px);
   overflow: hidden;
 `;
 
+export const Content = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: start;
+  padding: 5px 10px;
+  overflow: hidden;
+  transform-origin: center;
+  animation: ${clipAnimation} 0.8s ease-out 10ms;
+  transition: 0.8s;
+  filter: blur(0px);
+
+  &:hover {
+    .note_background {
+      transform: translateY(0px);
+    }
+  }
+`;
+
+export const NoteBackground = styled.div`
+position: absolute;
+  background-color: #ffe133;
+  width: 200px;
+  height: 250px;
+  margin-top: -5px;
+  transform: translateY(250px);
+  transition: 0.4s;
+`
+
 export const Title = styled.h2`
-  
+  z-index: 1;
 `;
 
 export const Text = styled.p`
-  
+  z-index: 1;
 `;
