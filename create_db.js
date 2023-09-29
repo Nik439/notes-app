348886ac71473d78b7a74e0e5ef33176f479514e
Nikos Cocci <coccinikos@gmail.com>
2023-09-29 19:08:20 +0200

    updated create_db script

--- create_db.js
+++ create_db.js
@@ -25,62 +25,25 @@ db.serialize(()=>{
         console.log("Created notes table.");
         db.run(
           `DELETE FROM notes`,
-          (err) => {
+          async (err) => {
             if (err)
               console.error(err.message);
             else {
               console.log("All rows deleted from notes");
   
-              const rows = [
-                [
-                  "test title 1",
+              for (let i=0; i<30; i++) {
+                await insert([
+                  "Test title " + (i+1),
                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex sed nisl feugiat suscipit. Mauris nunc tortor, scelerisque sed consectetur non, imperdiet quis est. Aliquam consequat erat ante, vel faucibus risus laoreet quis. Suspendisse varius justo vehicula, pulvinar quam quis, scelerisque ligula. Suspendisse metus dolor, sodales at vehicula sed, porttitor eu libero. Sed augue metus, scelerisque eu luctus vel, vestibulum non eros. Aenean sodales dolor vel turpis fermentum consectetur. Mauris iaculis velit quis tortor laoreet, quis convallis justo venenatis.",
                   new Date().toISOString(),
-                ],
-                [
-                  "test title 2",
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex sed nisl feugiat suscipit. Mauris nunc tortor, scelerisque sed consectetur non, imperdiet quis est. Aliquam consequat erat ante, vel faucibus risus laoreet quis. Suspendisse varius justo vehicula, pulvinar quam quis, scelerisque ligula. Suspendisse metus dolor, sodales at vehicula sed, porttitor eu libero. Sed augue metus, scelerisque eu luctus vel, vestibulum non eros. Aenean sodales dolor vel turpis fermentum consectetur. Mauris iaculis velit quis tortor laoreet, quis convallis justo venenatis.",
-                  new Date().toISOString(),
-                ],
-                [
-                  "test title 3",
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex sed nisl feugiat suscipit. Mauris nunc tortor, scelerisque sed consectetur non, imperdiet quis est. Aliquam consequat erat ante, vel faucibus risus laoreet quis. Suspendisse varius justo vehicula, pulvinar quam quis, scelerisque ligula. Suspendisse metus dolor, sodales at vehicula sed, porttitor eu libero. Sed augue metus, scelerisque eu luctus vel, vestibulum non eros. Aenean sodales dolor vel turpis fermentum consectetur. Mauris iaculis velit quis tortor laoreet, quis convallis justo venenatis.",
-                  new Date().toISOString(),
-                ]
-            ]
-  
-              db.run(
-                `INSERT INTO notes(title, text, last_update) VALUES(?, ?, ?)`,
-                rows[0],
-                (err) => {
-                  if (err)
-                    console.error(err.message);
-                  else
-                    console.log(`Row inserted`);
-                }
-              )
-  
-              db.run(
-                `INSERT INTO notes(title, text, last_update) VALUES(?, ?, ?)`,
-                rows[1],
-                (err) => {
-                  if (err)
-                    console.error(err.message);
-                  else
-                    console.log(`Row inserted`);
-                }
-              )
-  
-              db.run(
-                `INSERT INTO notes(title, text, last_update) VALUES(?, ?, ?)`,
-                rows[2],
-                (err) => {
-                  if (err)
-                    console.error(err.message);
-                  else
-                    console.log(`Row inserted`);
-                }
-              )
+                ])
+              }
+
+              await insert([
+                "Test with very very long title!!",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ex sed nisl feugiat suscipit. Mauris nunc tortor, scelerisque sed consectetur non, imperdiet quis est. Aliquam consequat erat ante, vel faucibus risus laoreet quis. Suspendisse varius justo vehicula, pulvinar quam quis, scelerisque ligula. Suspendisse metus dolor, sodales at vehicula sed, porttitor eu libero. Sed augue metus, scelerisque eu luctus vel, vestibulum non eros. Aenean sodales dolor vel turpis fermentum consectetur. Mauris iaculis velit quis tortor laoreet, quis convallis justo venenatis.",
+                new Date().toISOString(),
+              ])
   
               db.close((err) => {
                 if (err) 
@@ -96,3 +59,21 @@ db.serialize(()=>{
     }
   )
 })
+
+function insert(params) {
+  return new Promise((resolve, reject) => {
+    db.run(
+      `INSERT INTO notes(title, text, last_update) VALUES(?, ?, ?)`,
+      params,
+      (err) => {
+        if (err) {
+          console.error("DB Error: Insert failed: ", err.message);
+          return reject(err.message);
+        } else {
+          console.log(`Row inserted`);
+          return resolve("done");
+        }          
+      }
+    )
+  });
+}
